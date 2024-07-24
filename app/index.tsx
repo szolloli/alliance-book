3a69d8f4c642040359a14b1e730645459600dc83
szolloli <szolloli@fit.cvut.cz>
2024-07-24 17:52:01 +0200

    feat: create basic router structure

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>This is home screen</Text>
+    </View>
+  );
+}
