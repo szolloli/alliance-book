8b341984e8130c63c2915cb26d92a47720cef206
szolloli <szolloli@fit.cvut.cz>
2024-07-30 00:19:39 +0200

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: ['expo', 'prettier'],
+  extends: ['expo', 'eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'react/no-unused-prop-types': 'error',
   },
 };
